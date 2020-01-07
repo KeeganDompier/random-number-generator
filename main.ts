b7bebb00e1ddf3c80e760fa6f4239da3dbb54ac8
KeeganDompier <58610698+KeeganDompier@users.noreply.github.com>
2020-01-07 13:37:42 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+let _15 = Math.randomRange(1, 5)
+game.splash("This is a number between 1-5" + ":" + _15)
+let _510 = Math.randomRange(5, 10)
+game.splash("This is a number between 5-10" + ":" + _510)
+let _1020 = Math.randomRange(10, 20)
+game.splash("This is a number between 10-20" + ":" + _1020)
+let _1100 = Math.randomRange(1, 100)
+game.splash("This is a number between 1-100" + ":" + _1100)
